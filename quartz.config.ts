0f4f6614e920cada47e97903fa6cdf04ed1ebc58
DrAShanker <aparajeya247shanker@gmail.com>
2025-12-22 19:50:35 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,12 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        typography: {
+		header: "Averia Serif Libre",
+		body: "Averia Serif Libre",
+		code: "IBM Plex Mono",
+},
+
       },
       colors: {
         lightMode: {
